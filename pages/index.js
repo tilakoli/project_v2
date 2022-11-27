635fff2430865156350a72e4240ab89c227de7b5
Tilak oli <tilak.oli@madpaws.com.au>
2022-11-27 16:05:29 +0545

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from "next/head";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Shell</title>
+      </Head>
+      <h1 className="flex items-center justify-center w-full h-screen bg-orange-400">
+        Shell...
+      </h1>
+    </div>
+  );
+}
