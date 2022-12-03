15c188af46fc7fa51ccb735732622d363fd63816
Tilak oli <tilak.oli@madpaws.com.au>
2022-12-03 15:42:28 +0545

    homescreen mobile screen height test 2

--- Components/Homepage/index.js
+++ Components/Homepage/index.js
@@ -7,7 +7,7 @@ const Index = () => {
     <div>
       {/* <CustomCursor cursorVariant={cursorVariant} isHidden={isCursorHidden} /> */}
       <div className="flex flex-col md:flex-row ">
-        <div className="md:w-[90%] flex flex-col items-center justify-center h-screen md:h-screen text-2xl text-white md:text-5xl md:pr-56">
+        <div className="md:w-[90%] flex flex-col items-center justify-center h-[calc(100vh-2.8rem)] md:h-screen text-2xl text-white md:text-5xl md:pr-56">
           <h1 className="font-bold md:text-[70px] mb-2">Tilak Oli</h1>
           <div className="flex md:text-[30px]">
             I am a D
