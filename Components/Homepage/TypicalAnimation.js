4f594ef1412b307d17d50ce9715624ab93a9623d
Tilak oli <tilak.oli@madpaws.com.au>
2023-01-01 17:37:53 +0545

    fucking build error

--- Components/Homepage/TypicalAnimation.js
+++ Components/Homepage/TypicalAnimation.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
-import Typical from "react-typical";
+// import Typical from "react-typical";
 
 const TypicalAnimation = () => {
   return (
--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,22 @@ import { ThemeProvider } from "next-themes";
 function MyApp({ Component, pageProps, router }) {
   return (
     <ThemeProvider enableSystem={true} attribute="class">
-      <motion.div>
+      <motion.div
+        key={router.route}
+        // initial="initialPageLoad"
+        // animate="pageAnimate"
+        // variants={{
+        //   initialPageLoad: {
+        //     opacity: 0,
+        //   },
+        //   pageAnimate: {
+        //     opacity: 1,
+        //     transition: {
+        //       duration: 0.5,
+        //     },
+        //   },
+        // }}
+      >
         <Component {...pageProps} />
       </motion.div>
     </ThemeProvider>
