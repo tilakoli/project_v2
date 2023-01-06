4df9b4269f27cce1e127d1fe1cad3149c2279f75
Tilak oli <tilak.oli@madpaws.com.au>
2023-01-06 13:53:05 +0545

    check build failure 2

--- Components/Elements/Loader.js
+++ Components/Elements/Loader.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { ScatterBoxLoader } from "react-awesome-loaders";
+// import { ScatterBoxLoader } from "react-awesome-loaders";
 
 const Loader = () => {
   return (
@@ -7,7 +7,9 @@ const Loader = () => {
       className="fixed top-0 left-0 flex flex-col items-center justify-center w-screen h-screen gap-5 bg-black"
       style={{ zIndex: 9999999999 }}
     >
-      <ScatterBoxLoader primaryColor={"#999696"} background={"#999696"} />
+      Loader
+      {/* <ScatterBoxLoader primaryColor={"#999696"} background={"#999696"} />
+       */}
     </div>
   );
 };
