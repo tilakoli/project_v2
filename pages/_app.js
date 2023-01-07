eccb573c3a8db0e8cc1dcc50248fe8d0f6a61465
tilakoli <tilak.81oli@gmail.com>
2023-01-07 22:39:52 +0545

    loaderr removed

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,22 @@
 import React, { useState } from "react";
 import { motion } from "framer-motion";
 import { ThemeProvider } from "next-themes";
-import Loader from "../Components/Elements/Loader";
-import Router from "next/router";
+// import Loader from "../Components/Elements/Loader";
+// import Router from "next/router";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  const [loading, setLoading] = useState(false);
+  // const [loading, setLoading] = useState(false);
 
-  Router.events.on("routeChangeStart", (url) => {
-    setLoading(true);
-  });
-  Router.events.on("routeChangeComplete", (url) => {
-    setLoading(false);
-  });
+  // Router.events.on("routeChangeStart", (url) => {
+  //   setLoading(true);
+  // });
+  // Router.events.on("routeChangeComplete", (url) => {
+  //   setLoading(false);
+  // });
   return (
     <>
-      {loading ? <Loader /> : null}
+      {/* {loading ? <Loader /> : null} */}
 
       <ThemeProvider enableSystem={true} attribute="class">
         <motion.div>
