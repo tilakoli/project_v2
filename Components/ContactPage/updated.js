8848858e7f807b60c8f4a777cae7cec2d79fd57b
tilakoli <tilak.oli@gmail.com>
2023-01-25 16:12:23 +0545

    contact page  styles updated

--- Components/ContactPage/updated.js
+++ Components/ContactPage/updated.js
@@ -48,15 +48,16 @@ const Index = () => {
       window.location.reload();
     }, 500);
   };
-
   return (
-    <div className="z-50 flex flex-col items-center justify-center h-screen min-w-full text-black bg-customWhite dark:bg-primaryBlack/50 mt-14 md:mt-0 dark:text-white">
-      <CustomCursor variants={variants} animationVariants={cursorVariant} />
+    <div className="z-50 flex flex-col items-center justify-center h-screen min-w-full text-black mt-14 md:mt-0 bg-customWhite dark:bg-red-500 dark:text-white dark:backdrop-blur-sm">
+      <div className="hidden md:block">
+        <CustomCursor variants={variants} animationVariants={cursorVariant} />
+      </div>
       <div className="flex flex-col items-center justify-center gap-2 mt-12 text-3xl font-bold h-28">
         Contact {/*{infinityVector} */}
         <div className="w-full border border-customBlue"></div>
       </div>
-      <div className="flex flex-col items-center justify-center gap-5 p-5 rounded-lg md:gap-20 w-[90%]  md:w-2/3 backdrop-blur-sm bg-black/50 backdropFilter h-96">
+      <div className="flex flex-col items-center justify-center w-full gap-5 p-5 rounded-lg bg-black/50 md:gap-20 md:w-2/3 backdrop-blur-sm backdropFilter h-96">
         <div className="flex flex-col items-center justify-center w-full gap-5 md:flex-row">
           <input
             required
@@ -76,7 +77,7 @@ const Index = () => {
         <input
           required
           type="textarea"
-          className="w-full p-4 bg-transparent border-b outline-none h-96 focus:border-b-0 focus:border-l focus:duration-100 focus:transition-all md:w-full"
+          className="w-full p-4 bg-transparent border-b outline-none h-96 focus:border-b-0 focus:border-l focus:duration-100 focus:transition-all "
           placeholder="Your Message"
           onChange={(e) => setMessage(e.target.value)}
         />
@@ -85,7 +86,7 @@ const Index = () => {
         </div>
       </div>
       <div
-        className="flex flex-col w-[90%] h-auto p-5 mt-12 rounded-lg md:p-0 backdrop-blur-sm bg-black/50 md:w-2/3 md:flex-row"
+        className="flex flex-col w-full h-auto p-5 mt-12 rounded-lg md:p-0 backdrop-blur-sm bg-black/50 md:w-2/3 md:flex-row"
         onMouseEnter={onMouseEnter}
         onMouseLeave={onMouseLeave}
       >
