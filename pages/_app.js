121016cfcf7dcaafe1df6f60810691bb73bf30dc
tilakoli <tilak.oli@gmail.com>
2023-01-25 12:08:13 +0545

    fix prod build

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { motion } from "framer-motion";
 import { ThemeProvider } from "next-themes";
 import "../styles/globals.css";
