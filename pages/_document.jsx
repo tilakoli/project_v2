a280c745619d110aec7bf93f811c1ccc0a4ae22e
tilakoli <tilak.oli@gmail.com>
2023-02-04 20:43:05 +0545

    update for seo after testing with lighthouse

---
+++ pages/_document.jsx
@@ -0,0 +1,18 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <title>Tilak Oli</title>
+        <meta name="description" content="Portfolio of a web developer" />
+        <meta name="google" content="notranslate" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,9 @@
-import Head from "next/head";
 import Layout from "../Layout";
 import Homepage from "../Components/Homepage";
 
 export default function Home() {
   return (
     <div>
-      <Head>
-        <title>Tilak Oli</title>
-        <meta name="google" content="notranslate" />
-      </Head>
       <Layout>
         <Homepage />
       </Layout>
