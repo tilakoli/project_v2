01aebeaa18f877b929bc1f14d5a069f085cc5518
tilakoli <tilak.oli@madpaws.com.au>
2023-05-30 21:39:55 +0545

    update about page animation and add package

--- Components/Elements/DetailsContainer.tsx
+++ Components/Elements/DetailsContainer.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { motion } from "framer-motion";
 import { MdArrowForwardIos } from "react-icons/md";
 import { ProgressBar } from "../Progressbar";
 
@@ -10,38 +11,40 @@ type Props = {
 };
 
 const DetailsContainer = ({ label, key, value, type = "details" }: Props) => {
-  if (type === "details") {
-    return (
-      <div className="flex flex-col gap-2 md:gap-3" key={key}>
-        <div className="flex gap-2 text-lg md:text-xl">
-          <div className="">{label}:</div>
-          <div>{value}</div>
-        </div>
-      </div>
-    );
-  }
-  if (type === "bullet") {
-    return (
-      <div className="flex flex-col gap-2 cursor-pointer md:gap-3" key={key}>
-        <div className="flex items-center gap-2 text-lg capitalize md:text-xl">
-          <div className="">
-            <MdArrowForwardIos />
+  switch (type) {
+    case "details":
+      return (
+        <motion.div className="flex flex-col gap-2 md:gap-3" key={key}>
+          <div className="flex gap-2 text-lg md:text-xl">
+            <div className="">{label}:</div>
+            <div>{value}</div>
           </div>
-          <div>{label}</div>
-        </div>
-      </div>
-    );
-  } else {
-    return (
-      <div className="flex flex-col w-full gap-2 md:gap-3" key={key}>
-        <div className="flex items-center justify-start w-full gap-8 mb-3 text-lg md:text-xl">
-          <div className="flex items-center ">{label}:</div>
-          <div className="flex items-center justify-start w-1/2">
-            <ProgressBar width={value} />
+        </motion.div>
+      );
+    case "bullet":
+      return (
+        <motion.div className="flex flex-col gap-2 md:gap-3" key={key}>
+          <div className="flex items-center gap-2 text-lg capitalize md:text-xl">
+            <div className="">
+              <MdArrowForwardIos />
+            </div>
+            <div>{label}</div>
           </div>
-        </div>
-      </div>
-    );
+        </motion.div>
+      );
+    case "skills":
+      return (
+        <motion.div className="flex flex-col gap-2 md:gap-3" key={key}>
+          <div className="flex items-center justify-start w-full gap-8 mb-3 text-lg md:text-xl">
+            <div className="flex items-center ">{label}:</div>
+            <div className="flex items-center justify-start w-1/2">
+              <ProgressBar width={value} />
+            </div>
+          </div>
+        </motion.div>
+      );
+    default:
+      return null;
   }
 };
 
--- pages/About.tsx
+++ pages/About.tsx
@@ -1,34 +1,45 @@
 import React from "react";
 import { motion } from "framer-motion";
+import { ToastContainer } from "react-toastify";
 import Layout from "../Layout/index";
 import Badge from "../Components/Elements/Badge";
 import { Interests, description1, description2 } from "../utils/PersonalData";
 import DetailsContainer from "../Components/Elements/DetailsContainer";
-import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Signature from "../Components/Elements/Signature";
 import { useTheme } from "next-themes";
 import SkillsSphere from "../Components/SkillsSphere/SkillsSphere";
+import { useInView } from "react-intersection-observer";
+
+const SectionHeading = ({ sectionTitle }) => (
+  <h1 className="pb-2 text-2xl font-bold underline md:text-4xl">
+    {sectionTitle}
+  </h1>
+);
 
 const About = () => {
   const { theme } = useTheme();
+  const signatureStrokeColor = theme === "light" ? "#C0C0C0" : "#797979";
 
-  const signatureStrokeColor = theme === "light" ? "black" : "white";
+  const descriptionVariants = {
+    initial: { y: -20, opacity: 0 },
+    animate: { y: 0, opacity: 1 },
+  };
 
-  const SectionHeading = (sectionTitle: string) => (
-    <h1 className="pb-2 text-2xl font-bold underline md:text-4xl ">
-      {sectionTitle}
-    </h1>
-  );
+  const descriptionTransition = {
+    duration: 1,
+  };
+  const [sectionRef, inView] = useInView({
+    triggerOnce: false, // Animation triggers only once
+  });
 
   return (
-    <div className="h-full">
-      <div className="hidden md:block"></div>
+    <>
+      <div className="hidden md:block" />
 
       <Layout>
         <div className="z-50 flex flex-col items-center justify-center h-full min-w-full text-black md:mb-12 bg-customWhite dark:bg-primaryBlack/50 dark:text-white">
           <div className="h-full w-full md:mt-16 rounded-md  lg:w-[80%] flex items-start flex-col pt-14 pb-8 px-5 md:px-10 gap-5 ">
-            {" "}
             <ToastContainer
               autoClose={4000}
               closeOnClick
@@ -40,46 +51,64 @@ const About = () => {
             <div className="relative w-full h-auto md:flex ">
               {/* eslint-disable-next-line @next/next/no-img-element */}
               <img
-                src="/profile4.jpg"
+                src="/profileImageno.png"
                 alt="User profile picture"
-                className="object-cover w-full h-full bg-no-repeat bg-contain md:w-1/2 md:h-[500px] opacity-50 "
+                className="object-cover w-full h-full bg-no-repeat -mt-24 bg-contain md:w-1/2 md:h-[450px] opacity-50"
               />
               <div className="flex flex-col w-full h-auto leading-5 tracking-tighter md:leading-normal md:tracking-normal md:w-1/2">
-                <div className="h-auto text-justify md:pt-0 md:p-4 md:text-start text-md md:text-lg xl:text-xl">
-                  <p>{description1}</p>
+                <motion.div className="h-auto text-justify md:pt-0 md:p-4 md:text-start text-md md:text-lg xl:text-xl">
+                  <motion.p
+                    variants={descriptionVariants}
+                    initial="initial"
+                    animate="animate"
+                    transition={descriptionTransition}
+                  >
+                    {description1}
+                  </motion.p>
                   <br />
-                  <p>{description2}</p>
-                  {/* <br /> */}
-                  {/* {description3} */}
-                </div>
+                  <motion.p
+                    variants={descriptionVariants}
+                    initial="initial"
+                    animate="animate"
+                    transition={{ ...descriptionTransition, delay: 0.7 }}
+                  >
+                    {description2}
+                  </motion.p>
+                </motion.div>
 
                 <div className="flex items-end justify-start mt-5 md:justify-end">
-                  <Signature
-                    strokeColor={theme ? signatureStrokeColor : "white"}
-                  />
+                  {theme && <Signature strokeColor={signatureStrokeColor} />}
                 </div>
               </div>
             </div>
             {/* -------------------------skills ------------------------ */}
             <motion.div className="flex flex-col items-center justify-center w-full mt-4 mb-8 text-white bg-black rounded-md dark:bg-gray-400/60 backdrop-blur-sm dark:text-black">
               <div className="flex items-center w-full pt-5 pl-5">
-                {SectionHeading("My TechStack")}
+                <SectionHeading sectionTitle="My TechStack" />
               </div>
               <SkillsSphere />
             </motion.div>
             {/* ------------------------------------Knowledge/Interests ------------------------------------ */}
-            <div className="flex flex-col w-full gap-4 p-8 mb-8 text-white bg-black rounded-md md:mt-4 md:flex-row md:gap-4 dark:bg-gray-400/60 backdrop-blur-sm dark:text-black">
+            <motion.div
+              className="flex flex-col w-full gap-4 p-8 mb-8 text-white bg-black rounded-md md:mt-4 md:flex-row md:gap-4 dark:bg-gray-400/60 backdrop-blur-sm dark:text-black"
+              ref={sectionRef} // Add the ref to the motion.div
+              initial={{ opacity: 0, y: -20 }} // Initial animation values
+              animate={{ opacity: inView ? 1 : 0, y: inView ? 0 : -20 }} // Animation values based on the inView status
+              transition={{ duration: 0.7, delay: 0.5 }} // Animation duration
+            >
               <div className="flex flex-col w-full h-full gap-2 md:gap-3 md:w-1/2">
-                <div>{SectionHeading("Interests")}</div>
-                {Interests.map((e, i) => {
-                  return <DetailsContainer key={i} label={e} type="bullet" />;
-                })}
+                <div>
+                  <SectionHeading sectionTitle="Interests" />
+                </div>
+                {Interests.map((e, i) => (
+                  <DetailsContainer key={i} label={e} type="bullet" />
+                ))}
               </div>
-            </div>
+            </motion.div>
           </div>
         </div>
       </Layout>
-    </div>
+    </>
   );
 };
 
