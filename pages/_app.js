975a390de508ffa6304c5855ef209eb4f2744bd1
tilakoli <tilak.oli@madpaws.com.au>
2023-05-31 23:04:37 +0545

    remove loader2

--- pages/_app.js
+++ pages/_app.js
@@ -3,19 +3,19 @@ import { motion } from "framer-motion";
 import { ThemeProvider } from "next-themes";
 import "../styles/globals.css";
 import { Analytics } from "@vercel/analytics/react";
-import Router from "next/router";
-import Loader from "../Components/Elements/Loader";
+// import Router from "next/router";
+// import Loader from "../Components/Elements/Loader";
 
 function MyApp({ Component, pageProps }) {
-  const [isLoading, setIsLoading] = React.useState(false);
-  Router.events.on("routeChangeStart", (url) => {
-    console.log("routeChangeStart", url);
-    setIsLoading(true);
-  });
-  Router.events.on("routeChangeComplete", (url) => {
-    console.log("routeChangeComplete", url);
-    setIsLoading(false);
-  });
+  // const [isLoading, setIsLoading] = React.useState(false);
+  // Router.events.on("routeChangeStart", (url) => {
+  //   console.log("routeChangeStart", url);
+  //   setIsLoading(true);
+  // });
+  // Router.events.on("routeChangeComplete", (url) => {
+  //   console.log("routeChangeComplete", url);
+  //   setIsLoading(false);
+  // });
   return (
     <>
       <ThemeProvider enableSystem={true} attribute="class">
